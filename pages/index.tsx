63223e8d9932dadc09fc218738bd9b054a3a5a09
Alexander Vygodchikov <avygodchikov@gmail.com>
2021-09-16 22:39:59 +0300

    Add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,12 +18,29 @@ const Home: NextPage = () => {
   return (
     <div className='h-screen w-screen'>
       <Head>
-        <title>Personal Page</title>
+        <title>Alex Vygodchikov</title>
         <meta
           name='description'
           content='Alexander Vygodchikov personal page'
         />
-        <link rel='icon' href='/favicon.ico' /> {/**TODO: favicon */}
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
       </Head>
 
       <div
