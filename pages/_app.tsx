530b5d791fe29026f7957ae0c3bcefecfb6d1db8
Alexander Vygodchikov <avygodchikov@gmail.com>
2021-10-05 21:50:25 +0300

    Add browser validation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import type { AppProps } from 'next/app'
 import { ContextProvider } from '../components/Context/Context'
 import '../styles/globals.css'
+import Bowser from 'bowser'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const isValidBrowser = Bowser.getParser(
+      window.navigator.userAgent
+    ).satisfies({ chrome: '>90' })
+
+    if (!isValidBrowser)
+      alert(
+        '\n\nWrong browser detected!\n\nCurrently full operation is guaranteed only in the Chrome browser version older than 90.\n\nSorry for inconvenience.'
+      )
+  }, [])
+
   return (
     <ContextProvider>
       <Component {...pageProps} />
